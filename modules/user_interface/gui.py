9c0e160cc66712bf13f252395de7197d5370946d
theoldonee <rouvaf123@gmail.com>
2025-10-29 23:13:22 +0400

    feat(gui): make live_stream_btn toogle detection

--- modules/user_interface/gui.py
+++ modules/user_interface/gui.py
@@ -160,4 +160,14 @@ class GUI:
 
     # changes state of predict variable
     def start_prediction(self):
-        self.predict = True
+        # check if predict is true and video frames are being captured
+        if self.predict and self.running:
+            self.predict = False
+            self.live_stream_btn.config_colours(activeBG="green4", activeFG="white",
+                                       bgcolour="SpringGreen3", fgcolour="white", hoverBG="green3", hoverFG="white")
+            self.live_stream_btn.config_text("Start Live Detection")
+        elif self.predict == False and self.running :
+            self.predict = True
+            self.live_stream_btn.config_colours(activeBG="firebrick4", activeFG="white", bgcolour="firebrick3",
+                                fgcolour="white", hoverBG="red", hoverFG="white")
+            self.live_stream_btn.config_text("Stop Live Detection")
